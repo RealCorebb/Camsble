c2eec96c1ac2009439b6d0b8220c304a8059ff4b
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2022-01-02 21:07:28 +0800

    Initial commit

---
+++ Camsble.ino
@@ -0,0 +1,100 @@
+#include <Preferences.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+Preferences preferences;
+
+unsigned int mode;
+unsigned int triggerTimes;
+bool deviceConnected = false;
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic){
+    std::string value = pCharacteristic->getValue();
+
+    if(value.length() > 0){
+
+      Serial.println("**********");
+      Serial.print("New value: ");
+      
+      for(int i=0; i < value.length(); i++){
+        Serial.print(value[i]);
+      }
+
+      Serial.println();
+      Serial.println("**********");
+      
+    }
+  }
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);  
+  preferences.begin("config", false);
+  mode = preferences.getUInt("mode", 0);  // MODE  0 输入触发模式  1 Schedule模式    2 B门模式
+  triggerTimes = preferences.getUInt("triggerTimes", 1);   // 0模式触发次数
+
+
+  //-BLE----------------------------//
+  // Create the BLE Device
+    BLEDevice::init("Camsble");
+
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    // Create a BLE Characteristic
+    pCharacteristic = pService->createCharacteristic(
+                        CHARACTERISTIC_UUID,
+                        BLECharacteristic::PROPERTY_READ   |
+                        BLECharacteristic::PROPERTY_WRITE  |
+                        BLECharacteristic::PROPERTY_NOTIFY |
+                        BLECharacteristic::PROPERTY_INDICATE
+                      );
+
+    pCharacteristic->setCallbacks(new MyCallbacks());
+
+    // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+    // Create a BLE Descriptor
+    pCharacteristic->addDescriptor(new BLE2902());
+
+    // Start the service
+    pService->start();
+
+    // Start advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(false);
+    pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+    BLEDevice::startAdvertising();
+    Serial.println("Waiting a client connection to notify...");
+  //
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println(mode);
+}
