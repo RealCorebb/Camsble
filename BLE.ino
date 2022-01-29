b0a73ec1c0625aa69b963eec52ae3bef5f91d665
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2022-01-29 12:21:59 +0800

    BLE Split to another ino, Preferences great

---
+++ BLE.ino
@@ -0,0 +1,86 @@
+//---------------------------------   BLE -_,-
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#define SERVICE_UUID        "fc1d6c97-b334-45bd-ad8e-964cf37fba58"
+
+BLECharacteristic modeCs("d6b1d851-f38b-4a40-ab1a-323dca8b59c0", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic triggerTimesCs("18a09d26-50df-4539-b0c5-5cc8190dfac3", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic triggerDelayCs("2286b0e0-0fd4-470d-b07d-c2a04cac41c3", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic interValCs("8d4f535a-ec08-4957-bde7-fe0d02fa235f", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic scheduleCs("f3ab231f-d13c-49bb-a2dc-94bfbb1237cf", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic bShutterCs("203e69eb-471b-40dc-8d75-7824e112165b", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+BLECharacteristic selfieDelayCs("62f876a1-fbe9-4524-b548-6c3d1df6c4ad", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+//---------------------------------
+
+//---------------------------------   Callback -_,-
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      mode = String(value.c_str()).toInt();
+      preferences.putInt("mode", mode);      
+      Serial.println(String("Change Mode to:") + mode);
+  }
+};
+//-----------------------------------
+
+
+//---------------------------------- Init BLE -_,-
+void initBLE(){
+  
+  Serial.println("Starting BLE work!");
+  BLEDevice::init("Camsble");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *camsbleService = pServer->createService(BLEUUID(SERVICE_UUID),30,0);
+
+  camsbleService->addCharacteristic(&modeCs);
+  modeCs.addDescriptor(new BLE2902());
+  
+  camsbleService->addCharacteristic(&triggerTimesCs);
+  camsbleService->addCharacteristic(&triggerDelayCs);
+  camsbleService->addCharacteristic(&interValCs);
+  camsbleService->addCharacteristic(&scheduleCs);
+  camsbleService->addCharacteristic(&bShutterCs);
+  camsbleService->addCharacteristic(&selfieDelayCs);
+
+  // Start the service
+  camsbleService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+// ----------------------- BLE init & notify -_,-
+
+  modeCs.setCallbacks(new MyCallbacks());
+  modeCs.setValue(String(mode).c_str());
+  modeCs.notify();
+
+  //triggerTimesCs.setCallbacks(new MyCallbacks());
+  triggerTimesCs.setValue(String(triggerTimes).c_str());
+  triggerTimesCs.notify();
+
+  //triggerDelayCs.setCallbacks(new MyCallbacks());
+  triggerDelayCs.setValue(String(triggerDelay).c_str());
+  triggerDelayCs.notify();
+
+  //interValCs.setCallbacks(new MyCallbacks());
+  interValCs.setValue(String(interVal).c_str());
+  interValCs.notify();
+
+  scheduleCs.setValue(String(schedule).c_str());
+  scheduleCs.notify();
+
+  bShutterCs.setValue(String(bShutter).c_str());
+  bShutterCs.notify();
+
+  selfieDelayCs.setValue(String(selfieDelay).c_str());
+  selfieDelayCs.notify();
+
+//------------------------
+}
--- Camsble.ino
+++ Camsble.ino
@@ -1,70 +1,42 @@
 // MODE  0 输入触发模式  1 Schedule模式    2 B门模式
-/*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updates by chegewara
-*/
 
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
+#include <Preferences.h>
+Preferences preferences;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+//------------------------------- GLOBAL -_,-
+int mode;
+int triggerTimes;
+int triggerDelay;
+int interVal;
+int schedule;
+int bShutter;
+int selfieDelay;
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+// -------------------------
 
-BLECharacteristic modeCs("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-BLEDescriptor modeDs(BLEUUID((uint16_t)0x2903));
-
-int testvalue = 0;
-
-
-class MyCallbacks: public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
-      Serial.print("Changed value = ");
-      //Serial.println(value.c_str());
-      testvalue = String(value.c_str()).toInt();
-      Serial.println(testvalue);
-  }
-};
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+ 
 
-  BLEDevice::init("Camsble");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *camsbleService = pServer->createService(SERVICE_UUID);
+  preferences.begin("camsble", false);
+  mode = preferences.getInt("mode", 0);
+  triggerTimes = preferences.getInt("triggerTimes", 1);
+  triggerDelay = preferences.getInt("triggerDelay", 0);
+  interVal = preferences.getInt("interVal", 5000);
+  schedule = preferences.getInt("schedule", 0);
+  bShutter = preferences.getInt("bShutter", 0);
+  selfieDelay = preferences.getInt("selfieDelay", 0);
 
-  camsbleService->addCharacteristic(&modeCs);
-  modeDs.setValue("Camsble Mode");
-  modeCs.addDescriptor(new BLE2902());
+  initBLE();
 
-  // Start the service
-  camsbleService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
-
-  modeCs.setCallbacks(new MyCallbacks());
-
-  BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(200);
+  delay(500);
   //char* sb = "sb";
-  testvalue++;
   //String(millis()).toCharArray(sb,4);
-  modeCs.setValue(String(testvalue).c_str());
-  modeCs.notify();
+  
 }
