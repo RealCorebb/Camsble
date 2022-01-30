0100af305e6f7492d85dd61b577991ab0123b751
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2022-01-30 14:11:46 +0800

    Button

--- Camsble.ino
+++ Camsble.ino
@@ -1,8 +1,14 @@
 // MODE  0 输入触发模式  1 Schedule模式    2 B门模式
 
 #include <Preferences.h>
+#include <Ticker.h>
+#include <EasyButton.h>
+
 Preferences preferences;
 
+#define shutterS1 10
+#define shutterS2 5
+#define inputPIN 19
 //------------------------------- GLOBAL -_,-
 int mode;
 int triggerTimes;
@@ -12,12 +18,17 @@ int schedule;
 int bShutter;
 int selfieDelay;
 
+Ticker delayTimer;
+
+EasyButton inputButton(inputPIN);
 // -------------------------
 
 
 void setup() {
   Serial.begin(115200);
- 
+  
+  pinMode(shutterS1,OUTPUT);
+  pinMode(shutterS2,OUTPUT);
 
   preferences.begin("camsble", false);
   mode = preferences.getInt("mode", 0);
@@ -28,19 +39,46 @@ void setup() {
   bShutter = preferences.getInt("bShutter", 0);
   selfieDelay = preferences.getInt("selfieDelay", 0);
 
+  inputButton.begin();
+  inputButton.onPressedFor(0,inputTrigger);  
+
   initBLE();
+
+  if (inputButton.supportsInterrupt())
+  {
+    inputButton.enableInterrupt(inputISR);
+    Serial.println("Button will be used through interrupts");
+  }
 
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
-void inputTrigger(){
-  
+void inputISR()
+{
+  //When button is being used through external interrupts, parameter INTERRUPT must be passed to read() function
+  inputButton.read(); 
 }
+
+void triggerShutter(){
+  Serial.println("Shutter Triggered");
+  digitalWrite(shutterS1,LOW);
+  delay(50);
+  digitalWrite(shutterS1,HIGH);
+}
+
+void inputTrigger(){
+  if(mode == 0){
+    Serial.println("Input Triggered");
+    delayTimer.once(triggerDelay,triggerShutter);
+  }
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
+  //delay(50);
+  //Serial.println(digitalRead(3));
   //char* sb = "sb";
   //String(millis()).toCharArray(sb,4);
-  
+  inputButton.update();
 }
