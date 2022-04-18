26e4ab0ce1df49bf31cb145ed78ac7d27a121f2e
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2022-04-19 00:26:32 +0800

    mark

--- BLE.ino
+++ BLE.ino
@@ -176,51 +176,39 @@ class otaCallback: public BLECharacteristicCallbacks {
 //-----------------------------------
 
 static const uint8_t hidReportDescriptor[] = {
-      USAGE_PAGE(1),      0x01,       // Generic Desktop Ctrls
-      USAGE(1),           0x06,       // Keyboard
-      COLLECTION(1),      0x01,       // Application
-      REPORT_ID(1),       0x01,        //   Report ID (1)
-      USAGE_PAGE(1),      0x07,       //   Kbrd/Keypad
-      USAGE_MINIMUM(1),   0xE0,
-      USAGE_MAXIMUM(1),   0xE7,
-      LOGICAL_MINIMUM(1), 0x00,
-      LOGICAL_MAXIMUM(1), 0x01,
-      REPORT_SIZE(1),     0x01,       //   1 byte (Modifier)
-      REPORT_COUNT(1),    0x08,
-      HIDINPUT(1),           0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x01,       //   1 byte (Reserved)
-      REPORT_SIZE(1),     0x08,
-      HIDINPUT(1),           0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x06,       //   6 bytes (Keys)
-      REPORT_SIZE(1),     0x08,
-      LOGICAL_MINIMUM(1), 0x00,
-      LOGICAL_MAXIMUM(1), 0x65,       //   101 keys
-      USAGE_MINIMUM(1),   0x00,
-      USAGE_MAXIMUM(1),   0x65,
-      HIDINPUT(1),           0x00,       //   Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x05,       //   5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
-      REPORT_SIZE(1),     0x01,
-      USAGE_PAGE(1),      0x08,       //   LEDs
-      USAGE_MINIMUM(1),   0x01,       //   Num Lock
-      USAGE_MAXIMUM(1),   0x05,       //   Kana
-      HIDOUTPUT(1),          0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-      REPORT_COUNT(1),    0x01,       //   3 bits (Padding)
-      REPORT_SIZE(1),     0x03,
-      HIDOUTPUT(1),          0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-      END_COLLECTION(0)
+        USAGE_PAGE(1),      0x0C,          // USAGE_PAGE (Consumer)
+        USAGE(1),           0x01,          // USAGE (Consumer Control)
+        COLLECTION(1),      0x01,          // COLLECTION (Application)
+        USAGE_MINIMUM(1),   0x00,          //   USAGE_MINIMUM (Unassigned)
+        USAGE_MAXIMUM(2),   0x3c, 0x02,              //   USAGE_MAXIMUM (AC Format
+        REPORT_ID(1),       0x01, //   REPORT_ID (3)
+        USAGE_PAGE(1),      0x0C,          //   USAGE_PAGE (Consumer)
+        LOGICAL_MINIMUM(1), 0x00,          //   LOGICAL_MINIMUM (0)
+        LOGICAL_MAXIMUM(2), 0x3c, 0x02,    //   LOGICAL_MAXIMUM (1)
+        REPORT_SIZE(1),     0x10,          //   REPORT_SIZE (1)
+        REPORT_COUNT(1),    0x10,          //   REPORT_COUNT (16)
+        HIDINPUT(1),        0x00,          //   INPUT (Data,Var,Ab
     };
 
 uint8_t buttons = 0;
 
 void bleTriggerShutter(){
+  /*
+  const char* hello = "\n";
+  KEYMAP map = keymap[(uint8_t)*hello];
+  uint8_t msg[] = {map.modifier || buttons, 0x0, map.usage, 0x0,0x0,0x0,0x0,0x0};
+  Serial.println(map.modifier);
+  Serial.println(0x80);*/
   if(isConnected){
     Serial.println("BLE Trigger Shutter");
-
-      uint8_t msg[] = {0x0, 0x0, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0};
-      input->setValue(msg, sizeof(msg));
+      
+      uint8_t vol_up_key_press = 0xe9;
+      uint8_t vol_up_key_release = 0x0;
+      //uint8_t msg[] = {0x0, 0x0, 0xE9, 0x0, 0x0, 0x0, 0x0, 0x0};
+      input->setValue((uint8_t*)&vol_up_key_press,sizeof(vol_up_key_press));
       input->notify();
-      uint8_t msg1[] = {0x0, 0x0, 0x0, 0x0,0x0,0x0,0x0,0x0};
-      input->setValue(msg1,sizeof(msg1));
+      //uint8_t msg1[] = {0x0, 0x0, 0x0, 0x0,0x0,0x0,0x0,0x0};
+      input->setValue((uint8_t*)&vol_up_key_release,sizeof(vol_up_key_release));
       input->notify();
 
   }
@@ -278,7 +266,7 @@ void initBLE(){
   //---------------------------  HID -_,-
   hid = new BLEHIDDevice(pServer);
   input = hid->inputReport(1); // <-- input REPORTID from report map
-  output = hid->outputReport(1); // <-- output REPORTID from report map
+  output = hid->outputReport(0x01); // <-- output REPORTID from report map
 
   output->setCallbacks(new MyOutputCallbacks());
 
